ac3b25669ff534729fcf7cdeb125e2d0e2d52e2d
gowthamazmeera <gowthamazmeera@gmail.com>
2024-08-08 15:54:14 +0530

    tables updated here

--- src/components/pages/Addfirm.jsx
+++ src/components/pages/Addfirm.jsx
@@ -2,8 +2,40 @@ import React from 'react'
 
 function Addfirm() {
   return (
-    <div className='Addfirm'>
-        <h1>Addfirm</h1>
+    <div className='Addfirm-table'>
+        <form className='Addfirm'>
+          <h1>Add firm</h1>
+          <input type='text' placeholder='FirmName' />
+          <br></br>
+          <br></br>
+          <input type='text' placeholder='Area' />
+          <br></br>
+          <br></br>
+          <span>
+          <h6>Category:</h6>
+          <input type='checkbox' />veg
+          <input type='checkbox' />non-veg
+          </span>
+          <br></br>
+          <br></br>
+          <span>
+          <h6>Region:</h6>
+          <input type='checkbox' />south-indian
+          <input type='checkbox' />north-indian
+          <input type='checkbox' />chiness
+          <input type='checkbox' />bekari
+          </span>
+          <br></br>
+          <br></br>
+          <h6>offer:</h6>
+          <input type='text' placeholder='Example:30% off today' />
+          <div className='btn-submit'>
+          <button type='submit'>
+            submit
+          </button>
+        </div>
+
+        </form>
     </div>
   )
 }
--- src/components/pages/Addproduct.jsx
+++ src/components/pages/Addproduct.jsx
@@ -3,7 +3,33 @@ import React from 'react'
 function Addproduct() {
   return (
     <div className='Addproduct'>
-        <h1>Addproduct</h1>
+        <form className='Addfirm'>
+          <h1>Add Product</h1>
+          <input type='text' placeholder='Add product' />
+          <br></br>
+          <br></br>
+          <input type='text' placeholder='price' />
+          <br></br>
+          <br></br>
+          <span>
+          <h6>Category:</h6>
+          <input type='checkbox' />veg
+          <input type='checkbox' />non-veg
+          </span>
+          <br></br>
+          <br></br>
+          <input type='file'  />
+          <br></br>
+          <br></br>
+          <h6>best seller:</h6>
+          <input type='text' placeholder='best seller' />
+          <div className='btn-submit'>
+          <button type='submit'>
+            submit
+          </button>
+        </div>
+
+        </form>
     </div>
   )
 }
